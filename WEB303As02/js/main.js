4616c6d63e182ff3636eee1924322bd9f7abddca
studbury <W0726026@myscc.ca>
2022-10-03 18:05:30 -0400

    Complete Assignment w/ animations
    uses xhr javascript to make requests

--- WEB303As02/js/main.js
+++ WEB303As02/js/main.js
@@ -2,19 +2,76 @@
 /* Andrew Breshamer
 0726026
 2022-10-03 */
-$(document).ready(function() {
-    var xhr = new XMLHttpRequest;
+$(function() {
+    var xhr = new XMLHttpRequest; //browser object that doesn't depend on jquery
+    let selectedElement = document.getElementById('content'); //select div for insertion
+    selectedElementJ = $('div#content');
     $('#prospect').on('click', function() {
-        //console.log("click event works");
-        /* console.log(xhr);
-        xhr.open('GET','convert.html',true);
-        console.log(xhr.responseText);
+        //OG javascript
+        //console.log(xhr);
+        xhr.open('GET','prospect.html',true); //sends HTTP request to server
+        //console.log(xhr.responseText);
         xhr.onload = function() {
             if(this.status === 200) {
-                let selectedElement = document.getElementById('#content');
-                selectedElement.innerHTML = xhr.responseText;
+                //console.log(selectedElement);
+                selectedElement.innerHTML = xhr.responseText; //insert html
             }
         }
-        xhr.send(); */
+        xhr.send();
+
+        /* selectedElementJ.load("prospect.html", function(responseTxt, statusTxt, xhr) {
+            //jquery
+            if(statusTxt == "success") {
+                console.log(".load() request was succesfully executed");
+                console.log(xhr);
+            }
+            if(statusTxt == "error") {
+                console.log(".load() request was not executed: " + xhr.status + ": " + xhr.statusText);
+            }
+        }); */
+
+        selectedElement.style.cssText  = "display: block; position: relative";
+        selectedElementJ.animate({
+            left: '0px',
+            opacity: '1'
+        });
+    });
+
+    $('#convert').on('click', function() {
+        //console.log(xhr);
+        xhr.open('GET','convert.html',true); //sends HTTP request to server
+        //console.log(xhr.responseText);
+        xhr.onload = function() {
+            if(this.status === 200) {
+                //console.log(selectedElement);
+                selectedElement.innerHTML = xhr.responseText; //insert html
+            }
+        }
+        xhr.send();
+
+        selectedElement.style.cssText  = "display: block; position: relative";
+        selectedElementJ.animate({
+            left: '0px',
+            opacity: '1'
+        });
+    });
+
+    $('#retain').on('click', function() {
+        //console.log(xhr);
+        xhr.open('GET','retain.html',true); //sends HTTP request to server
+        //console.log(xhr.responseText);
+        xhr.onload = function() {
+            if(this.status === 200) {
+                //console.log(selectedElement);
+                selectedElement.innerHTML = xhr.responseText; //insert html
+            }
+        }
+        xhr.send();
+
+        selectedElement.style.cssText  = "display: block; position: relative";
+        selectedElementJ.animate({
+            left: '0px',
+            opacity: '1'
+        });
     });
 });
