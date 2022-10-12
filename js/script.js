0d436a3417eb746bd0fd799bf457ee1f5f2d91d5
studbury <W0726026@myscc.ca>
2022-10-11 20:14:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ function RetrieveData() {
     $.getJSON("team.json", function(data) { //why not "..//team.json"?
         //console.log(data);
         $.each(data, function(i) {//loop through each object for JSON data
-            //console.log(data[i]);
-            $.each(data[i], function() { //loop through each child object for tag insertion
-                console.log(data);
+            let valueArray = data[i];
+            $.each(valueArray, function() { //loop through each child object for tag insertion
+                console.log(valueArray);
+                console.log(valueArray.name);
                 //$(`<h2>${}</h2>`).appendTo("div#team");
             });
         });
