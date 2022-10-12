c176e5177c5a78222ceac62128a3526a09adc52e
studbury <W0726026@myscc.ca>
2022-10-11 20:56:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,17 +5,22 @@ $(function() {
     RetrieveData();
 });
 
-function RetrieveData() {
+/* function RetrieveData() {
     $.getJSON("team.json", function(data) { //why not "..//team.json"?
         //console.log(data);
         $.each(data, function(i, currentArray) {//loop through each object for JSON data
-            console.log(i);
-            $.each(currentArray, function() { //loop through each child object for tag insertion
-                console.log(currentArray);
-                /* $(`<h2>${currentArray.name}</h2>`).appendTo("div#team");
-                $(`<h5>${currentArray.position}</h5>`).appendTo("div#team");
-                $(`<p>${currentArray.bio}</p>`).appendTo("div#team"); */
+            
+                
             });
+        });
+    });
+} */
+function RetrieveData() {
+    $.getJSON("team.json", function(data) {
+        $.each(data, function(i ,currentArray) {
+            $(`<h2>${currentArray.name}</h2>`).appendTo("div#team");
+            $(`<h5>${currentArray.position}</h5>`).appendTo("div#team");
+            $(`<p>${currentArray.bio}</p>`).appendTo("div#team");
         });
     });
 }
