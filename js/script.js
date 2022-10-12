ccfc2c3b311b602292a0fba4d5cfcdcd7393dfa5
studbury <W0726026@myscc.ca>
2022-10-11 20:17:49 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ function RetrieveData() {
         $.each(data, function(i) {//loop through each object for JSON data
             let valueArray = data[i];
             $.each(valueArray, function() { //loop through each child object for tag insertion
-                //console.log(valueArray);
-                console.log(valueArray.name);
-                //$(`<h2>${}</h2>`).appendTo("div#team");
+                $(`<h2>${valueArray.name}</h2>`).appendTo("div#team");
+                $(`<h5>${valueArray.position}</h5>`).appendTo("div#team");
+                $(`<p>${valueArray.bio}</p>`).appendTo("div#team");
             });
         });
     });
